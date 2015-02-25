47f330c68ab2b9aae9b805cede5effc3781d9471
Andrew Dawes <dawes@pacificu.edu>
2015-02-25 00:12:10 -0800

    set up for packaging

---
+++ rubidium/__init__.py
@@ -0,0 +1,1 @@
+from .rubidiumD2 import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rubidium',
+      version='0.1',
+      description='',
+      url='http://github.com/DawesLab/rubidium',
+      author='Andrew M.C. Dawes',
+      author_email='dawes@pacificu.edu',
+      license='GPLv3',
+      packages=['rubidium'],
+      zip_safe=False)
